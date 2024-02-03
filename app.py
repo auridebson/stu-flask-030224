3afe06a3168d1fdaadc325106719914537d396f4
Auridebson Lima <auridebson@vutus.com>
2024-02-03 19:22:24 -0300

    Update

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
 
+lista = ["Giovanni", "Maria", "Giuseppe", "Anna", "Luca", "Sofia", "Marco", "Giulia", "Alessandro", "Martina"]
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     nome = "auridebson"
     idade = 39
-    return render_template('index.html', nome=nome, idade=idade)
+    return render_template('index.html', nome=nome, idade=idade, lista=lista)
 
 if __name__ == "__main__":
     app.run(debug=True)
